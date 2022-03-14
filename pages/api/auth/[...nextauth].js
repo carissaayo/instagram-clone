914de8de4ec1f0b9ee6598b3aa63064c76803c7e
yussuf ajao <ajaoyussufayo@gmail.com>
2022-03-14 13:59:14 +0100

    added next_Secret code

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -8,8 +8,9 @@ export default NextAuth({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
-  ],
-
+  ]
+,
+  secret: process.env.NEXTAUTH_SECRET,
   pages: {
     signIn: "/auth/signin",
   },
